8579dd6034d49b14e8a232dd4d629e7edb68d8ac
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2024-01-03 15:20:28 +0000

    Installed leaflet

--- SunsetPro/src/components/Home.tsx
+++ SunsetPro/src/components/Home.tsx
@@ -1,5 +1,20 @@
+import { MapContainer, TileLayer, useMap, Marker, Popup } from 'react-leaflet'
+
 export const Home = () => {
     return (
-        <div>Helloooooooooooooooooooo! </div>
+        <div className='h-screen bg-gradient-to-r from-orange-300 to-rose-300' >
+            <h1 className=''>Choose your location</h1>
+            <MapContainer id="map" className="h-96 w-full" center={[39.5572, -7.51]} zoom={6} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[38.7077507, -9.1365919]}>
+                    <Popup>
+                        A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
     )
 }
