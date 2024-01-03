008471bcfa69a3e60ce72348df0501cb9a854ab0
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2024-01-03 14:34:05 +0000

    Added routing

--- SunsetPro/src/App.tsx
+++ SunsetPro/src/App.tsx
@@ -1,13 +1,20 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import { NavBar } from './components/NavBar';
+import { Home } from "./components/Home";
 
 
 function App() {
 
   return (
-    <>
-     <NavBar/>
-    </>
+    <div className="App">
+      <NavBar />
+      <main>
+        <Routes>
+          <Route path='/' element={<Home />} />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
--- SunsetPro/src/components/NavBar.tsx
+++ SunsetPro/src/components/NavBar.tsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const NavBar = () => {
     return (
         <>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="w-full flex flex-wrap items-center justify-between mx-auto p-4">
-                    <a
-                        href="https://flowbite.com/"
+                    <Link to="/"
                         className="flex items-center space-x-3 rtl:space-x-reverse"
                     >
                         <img
@@ -12,7 +13,7 @@ export const NavBar = () => {
                             className="h-12"
                             alt="Sunset Pro Logo"
                         />
-                    </a>
+                    </Link>
                     <div className="flex md:order-2">
                         <button
                             type="button"
@@ -90,7 +91,7 @@ export const NavBar = () => {
                         </button>
                     </div>
 
-                    
+
                     <div
                         className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                         id="navbar-search"
@@ -124,34 +125,31 @@ export const NavBar = () => {
                         </div>
                         <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                             <li>
-                                <a
-                                    href="#"
+                                <Link to="/"
                                     className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                                     aria-current="page"
                                 >
                                     Home
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                <a
-                                    href="#"
+                                <Link to=""
                                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Top
-                                </a>
+                                </Link>
                             </li>
                             <li>
-                                <a
-                                    href="#"
+                                <Link to=""
                                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
                                     Contribute
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </nav>
+                                </Link>
+                            </li >
+                        </ul >
+                    </div >
+                </div >
+            </nav >
 
         </>
     )
