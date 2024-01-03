8cdc6a8397cdf85fa154bb461b03ff049d30eb9a
Juliana <106109538+JulianaGeorgi@users.noreply.github.com>
2024-01-03 14:11:48 +0000

    added brand logo in the main nav

--- SunsetPro/src/components/NavBar.tsx
+++ SunsetPro/src/components/NavBar.tsx
@@ -8,13 +8,10 @@ export const NavBar = () => {
                         className="flex items-center space-x-3 rtl:space-x-reverse"
                     >
                         <img
-                            src="https://flowbite.com/docs/images/logo.svg"
-                            className="h-8"
-                            alt="Flowbite Logo"
+                            src="public/sunset-pro-high-resolution-logo-transparent.png"
+                            className="h-12"
+                            alt="Sunset Pro Logo"
                         />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-                            Flowbite
-                        </span>
                     </a>
                     <div className="flex md:order-2">
                         <button
@@ -64,7 +61,7 @@ export const NavBar = () => {
                                 type="text"
                                 id="search-navbar"
                                 className="block w-full p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                placeholder="Search..."
+                                placeholder="Enter location...."
                             />
                         </div>
                         <button
@@ -92,10 +89,14 @@ export const NavBar = () => {
                             </svg>
                         </button>
                     </div>
+
+                    
                     <div
                         className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
                         id="navbar-search"
                     >
+
+                        {/* Mobile Search Bar  */}
                         <div className="relative mt-3 md:hidden">
                             <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
                                 <svg
@@ -136,7 +137,7 @@ export const NavBar = () => {
                                     href="#"
                                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
-                                    About
+                                    Top
                                 </a>
                             </li>
                             <li>
@@ -144,7 +145,7 @@ export const NavBar = () => {
                                     href="#"
                                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                                 >
-                                    Services
+                                    Contribute
                                 </a>
                             </li>
                         </ul>
